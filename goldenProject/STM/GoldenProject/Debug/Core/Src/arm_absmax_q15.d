89b69661bcd9d15d3937b239e7a35a170dad5277
Julien Grolet <julien.grolet2@gmail.com>
2024-02-12 14:28:56 +0100

    Ajout du projet C

---
+++ goldenProject/STM/GoldenProject/Debug/Core/Src/arm_absmax_q15.d
@@ -0,0 +1,7 @@
+Core/Src/arm_absmax_q15.o: ../Core/Src/arm_absmax_q15.c \
+ /home/julien/STM32CubeIDE/workspace_1.13.2/GoldenProject/ARM_CMSIS/CMSIS/DSP/Include/arm_math.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h
+/home/julien/STM32CubeIDE/workspace_1.13.2/GoldenProject/ARM_CMSIS/CMSIS/DSP/Include/arm_math.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
---
+++ goldenProject/STM/GoldenProject/Debug/Core/Src/spectrogram.d
@@ -0,0 +1,14 @@
+Core/Src/spectrogram.o: ../Core/Src/spectrogram.c \
+ ../Core/Inc/spectrogram.h \
+ /home/julien/STM32CubeIDE/workspace_1.13.2/GoldenProject/ARM_CMSIS/CMSIS/DSP/Include/arm_math.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h ../Core/Inc/spectrogram_tables.h \
+ ../Core/Inc/config.h ../Core/Inc/utils.h ../Core/Inc/arm_absmax_q15.h
+../Core/Inc/spectrogram.h:
+/home/julien/STM32CubeIDE/workspace_1.13.2/GoldenProject/ARM_CMSIS/CMSIS/DSP/Include/arm_math.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Core/Inc/spectrogram_tables.h:
+../Core/Inc/config.h:
+../Core/Inc/utils.h:
+../Core/Inc/arm_absmax_q15.h:
