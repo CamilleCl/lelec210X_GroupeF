caae347620e7b80668aa1f4e0daad23b2504296c
Lucie <lucievanroy2@gmail.com>
2024-03-03 11:47:34 +0100

    random time for send message to uart

--- classification/audioUart.py
+++ classification/audioUart.py
@@ -8,6 +8,7 @@ then poetry run python audioUart.py -p PORT?
 import librosa  # For audio signal computations as MFCC
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import random
 import sounddevice as sd
 import soundfile as sf
 import serial
@@ -21,24 +22,27 @@ import time
 from classification.datasets import Dataset, get_cls_from_path
 from classification.utils.plots import plot_audio, plot_specgram
 
+SoundPerClasse = 20
 
-
-
-def playing_sound(port = None, classes = None):
+def playing_sound(SoundPerClasse, port = None, classes = None):
     classe = 'birds'
-    for i in range(20):
+    for i in range(SoundPerClasse):
             sound = dataset[classe, i]
             x, fs = sf.read(sound)
             # target_dB = 25
             # x /= np.linalg.norm(x) * 10 ** (-target_dB / 20)
             print(f'Playing a "{get_cls_from_path(sound)}"')
+            
+            sleeptime = random.uniform(0, 4)
+            print("sleeping for:", sleeptime, "seconds")
+            sleep(sleeptime)
 
             ser = serial.Serial(port = port, baudrate = 115200)
             ser.write(bytearray('s','ascii'))
         
             sd.play(x, fs)
 
-            time.sleep(7)
+            time.sleep(7-sleeptime)
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-p", "--port", help="Port for serial communication")
@@ -59,4 +63,4 @@ if args.port is None:
 else:
     dataset = Dataset()
     classes = dataset.list_classes()
-    playing_sound(port=args.port, classes = classes)
+    playing_sound(SoundPerClasse, port=args.port, classes = classes)
